07b6ac1f077fb8fdbf1a33becf25d0abc5710f6d
sinajahan <seenaah@gmail.com>
2013-03-19 14:12:18 -0400

    drive is now disconnected from mimetype

--- lib/file_convert.rb
+++ lib/file_convert.rb
@@ -3,5 +3,7 @@ require_relative 'drive'
 
 
 drive = Drive.new
-txt_url = drive.txt_url '/Users/sina/Downloads/Resume_Jane.pdf'
-puts txt_url
+txt_url1 = drive.txt_url '/Users/sina/Downloads/Resume_Jane.pdf'
+txt_url2 = drive.txt_url '/Users/sina/Downloads/My Resume.docx'
+puts txt_url1
+puts txt_url2
