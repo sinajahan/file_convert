956b954d805ca5427da69dc2a5770c20bc0a1d14
sinajahan <seenaah@gmail.com>
2013-03-28 11:13:20 -0400

    add a non s3 method

--- lib/file_convert.rb
+++ lib/file_convert.rb
@@ -12,6 +12,10 @@ module FileConvert
       @bucket = Bucket.new(params[:s3_key], params[:s3_secret], params[:s3_bucket_name], params[:system_folder])
     end
 
+    def to_txt(file_path)
+      @drive.get_txt file_path
+    end
+
     def to_txt_from_s3(s3_file_key)
       s3_file = @bucket.download s3_file_key
       @drive.get_txt s3_file.path
